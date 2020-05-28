4aa9100ff65d2fd394561c4ac7828eb547a77c24
Ryan Beiden <rdbeiden@gmail.com>
2020-05-28 19:42:32 -0400

    add bear array

--- javascripts/helpers/data/bearData.js
+++ javascripts/helpers/data/bearData.js
@@ -1,5 +1,12 @@
-const addBear = () {
+const bears = [
+  {
+    name: 'Test Bear',
+    imageUrl: './assets/navbar-logo@2x.png'
+  },
+];
 
+const addBear = () => {
+  return console.log(bears);
 }
 
 export default { addBear };
