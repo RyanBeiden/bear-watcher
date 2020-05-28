2e15c37abcf9472d199dea9cbb71293c5914c5d2
Ryan Beiden <rdbeiden@gmail.com>
2020-05-28 19:31:43 -0400

    create component & data files

---
+++ javascripts/components/bearForm.js
@@ -0,0 +1,9 @@
+import utils from '../helpers/utils.js';
+import bearData from '../helpers/data/bearData.js';
+import river from './river.js'; // maybe?
+
+const printForm = () => {
+  console.log(`Form Works!`);
+}
+
+export default { printForm };
