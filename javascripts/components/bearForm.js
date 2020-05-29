163a86fca598b47a9ec3b6c36f7368af350f7826
Ryan Beiden <rdbeiden@gmail.com>
2020-05-28 20:36:50 -0400

    add form printed to DOM

--- javascripts/components/bearForm.js
+++ javascripts/components/bearForm.js
@@ -1,9 +1,27 @@
 import utils from '../helpers/utils.js';
-import bearData from '../helpers/data/bearData.js';
-import river from './river.js'; // maybe?
+// import bearData from '../helpers/data/bearData.js';
+// import river from './river.js'; // maybe?
 
 const printForm = () => {
-  console.log(`Form Works!`);
-}
+  const domString = `
+    <div class="container bear-form">
+      <h2>Track a New Bear!</h2>
+      <form>
+        <div class="d-flex justify-content-center">
+          <div class="row">
+            <div class="form-group col">
+              <input type="text" class="form-control" placeholder="Bear's Name">
+            </div>
+            <div class="form-group col">
+              <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  `;
+  utils.printToDom('#bear-form', domString);
+};
 
 export default { printForm };
